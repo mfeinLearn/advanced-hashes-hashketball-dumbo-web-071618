67df762d27ae4ab09b3be22666e2eb4df5585683
Malcome Fein <mfein90@gmail.com>
2018-08-02 19:40:16 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -79,6 +79,7 @@ def scored_most_points
         if player_details_hash[:points] > answer
           answer = player_details_hash[:points]
           player_scored_most_points = player
+          binding.pry
         end 
       end
     end 
