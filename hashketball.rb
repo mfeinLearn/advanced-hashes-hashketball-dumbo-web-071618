81c1709b47797f9fd0d6af5da2da1beb6a7fb2eb
Malcome Fein <mfein90@gmail.com>
2018-08-02 17:52:26 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -10,30 +10,30 @@ require "pry"
       :team_name =>"Brooklyn Nets",
         :colors => ["Black","White"],
           :players => {
-          "Alan Anderson" => {number: "0", shoe: "16", points: "22",rebounds: "12", assists: "12", steals: "3", blocks: "1", slam_dunks: "1"  },
+          "Alan Anderson" => {number: 0, shoe: 16, points: 22,rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks: 1  },
           
-          "Reggie Evans" => {number: "30", shoe: "14", points: "12",rebounds: "12", assists: "12", steals: "12", blocks: "12", slam_dunks: "7"  },
+          "Reggie Evans" => {number: 30, shoe: 14, points: 12,rebounds: 12, assists: 12, steals: 12, blocks: 12, slam_dunks: 7  },
           
-          "Brook Lopez" => {number: "11", shoe: "17", points: "17",rebounds: "19", assists: "10", steals: "3", blocks: "1", slam_dunks: "15"  },
+          "Brook Lopez" => {number: 11, shoe: 17, points: 17,rebounds: 19, assists: 10, steals: 3, blocks: 1, slam_dunks: 15  },
           
-          "Mason Plumlee" => {number: "1", shoe: "19", points: "26",rebounds: "12", assists: "6", steals: "3", blocks: "8", slam_dunks: "5"  },
+          "Mason Plumlee" => {number: 1, shoe: 19, points: 26,rebounds: 12, assists: 6, steals: 3, blocks: 8, slam_dunks: 5  },
           
-          "Jason Terry" => {number: "31", shoe: "15", points: "19",rebounds: "2", assists: "2", steals: "4", blocks: "11", slam_dunks: "1"}
+          "Jason Terry" => {number: 31, shoe: 15, points: 19,rebounds: 2, assists: 2, steals: 4, blocks: 11, slam_dunks: 1}
             }
         },
     :away => {
       :team_name => "Charlotte Hornets",
         :colors => ["Turquoise","Purple"],
           :players => {
-          "Jeff Adrien" => {number: "4", shoe: "18", points: "10",rebounds: "1", assists: "1", steals: "2", blocks: "7", slam_dunks: "2"  },
+          "Jeff Adrien" => {number: 4, shoe: 18, points: 10,rebounds: 1, assists: 1, steals: 2, blocks: 7, slam_dunks: 2  },
           
-          "Bismak Biyombo" => {number: "0", shoe: "16", points: "12",rebounds: "4", assists: "7", steals: "7", blocks: "15", slam_dunks: "10"  },
+          "Bismak Biyombo" => {number: 0, shoe: 16, points: 12,rebounds: 4, assists: 7, steals: 7, blocks: 15, slam_dunks: 10  },
           
-          "DeSagna Diop" => {number: "2", shoe: "14", points: "24",rebounds: "12", assists: "12", steals: "4", blocks: "5", slam_dunks: "5"  },
+          "DeSagna Diop" => {number: 2, shoe: 14, points: 24,rebounds: 12, assists: 12, steals: 4, blocks: 5, slam_dunks: 5  },
           
-          "Ben Gordon" => {number: "8", shoe: "15", points: "33",rebounds: "3", assists: "2", steals: "1", blocks: "1", slam_dunks: "0"  },
+          "Ben Gordon" => {number: 8, shoe: 15, points: 33,rebounds: 3, assists: 2, steals: 1, blocks: 1, slam_dunks: 0  },
           
-          "Brendan Haywood" => {number: "33", shoe: "15", points: "6",rebounds: "12", assists: "12", steals: "22", blocks: "5", slam_dunks: "12"  }
+          "Brendan Haywood" => {number: 33, shoe: 15, points: 6,rebounds: 12, assists: 12, steals: 22, blocks: 5, slam_dunks: 12  }
         
       }
     }
@@ -136,7 +136,7 @@ def player_numbers(team_name)
 end
 
 def player_stats(name)
-  # answer = nil
+  answer = nil
 
   game_hash.each do |team, details_hash|
      #binding.pry
@@ -149,4 +149,35 @@ def player_stats(name)
       end
   end
   answer
+end
+
+# def big_shoe_rebounds
+#   game_hash.each do |team, details_hash|
+#     #binding.pry
+#     players_array = details_hash[:players]
+#       players_array.each do |player, player_details_hash|
+#         binding.pry
+#           player_details_hash.max_by {|k,v| v}
+#             #binding.pry
+#           #end
+#         #end
+#       end
+#   end
+# end
+
+
+def big_shoe_rebounds
+  # answer = nil
+
+  game_hash.each do |team, details_hash|
+    #binding.pry
+    players_array = details_hash[:players]
+      players_array.each do |player, player_details_hash|
+        #binding.pry
+          player_details_hash.each do |ke, va|
+            #binding.pry
+          end
+      end
+  end
+  # answer.to_i
 end
